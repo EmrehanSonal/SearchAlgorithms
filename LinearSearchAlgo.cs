819d9e9c36ec97b2f4b797e811888e0785b5c523
ErmeRG <emrehansonal@hotmail.com>
2026-04-24 19:24:08 +0300

    We made a for loop for travel all of the indexes and if we find it returns index.

--- LinearSearchAlgo.cs
+++ LinearSearchAlgo.cs
@@ -6,7 +6,17 @@ public class LinearSearchAlgo
     {
         if (numbers == null || numbers.Length == 0)
         {
-            
+            return -1;
         }
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+
     }
 }
