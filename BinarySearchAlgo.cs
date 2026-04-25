e154982837e5f10f06a9036f2616bdebe847bc3f
ErmeRG <emrehansonal@hotmail.com>
2026-04-25 15:18:30 +0300

    I added a if condition for array situtation

--- BinarySearchAlgo.cs
+++ BinarySearchAlgo.cs
@@ -4,6 +4,9 @@ public class BinarySearchAlgo
 {
     public static int BinarySearch(int[] numbers, int target)
     {
-        
+        if (numbers == null || numbers.Length == 0)
+        {
+            return -1;
+        }
     }
 }
