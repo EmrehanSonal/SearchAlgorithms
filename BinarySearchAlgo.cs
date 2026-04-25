5e1840a865d74c68f16627d24be78d873fae5189
ErmeRG <emrehansonal@hotmail.com>
2026-04-25 15:30:09 +0300

    I add if else condition for changing mid index due to our target

--- BinarySearchAlgo.cs
+++ BinarySearchAlgo.cs
@@ -21,7 +21,15 @@ public class BinarySearchAlgo
                 return mid;
             }
 
-        }
+            if (numbers[mid] < target)
+            {
+                left = mid + 1;
+            }else
+            {
+                right = mid - 1;
+            }
 
+        }
+        return -1;
     }
 }
