e37b3931bc6cb7a150771759557dc0a77f678cf0
ErmeRG <emrehansonal@hotmail.com>
2026-04-26 23:47:22 +0300

    I made a if condition for check array is null or not and empty or not

--- JumpSearchAlgo.cs
+++ JumpSearchAlgo.cs
@@ -5,5 +5,10 @@ public class JumpSearchAlgo
     public static int JumpSearch(int[] numbers, int target)
     {
         
+        if (numbers == null || numbers.Length == 0)
+        {
+            return -1;
+        }
+
     }
 }
