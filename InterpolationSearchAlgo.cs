8ae603ac39db1176a4735437763fb176526ff07d
ErmeRG <emrehansonal@hotmail.com>
2026-04-30 14:28:56 +0300

    I made a if condition for null and empty check for array.

--- InterpolationSearchAlgo.cs
+++ InterpolationSearchAlgo.cs
@@ -4,6 +4,16 @@ public class InterpolationSearchAlgo
 {
     public static int InterpolationSearch(int[] numbers, int target)
     {
+
+        if (numbers = null || numbers.Length == 0)
+        {
+            return -1;
+        }
+
         
+
+
+        return -1;
+
     }
 }
