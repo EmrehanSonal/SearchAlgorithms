cc6cc7d3fa9d0aef9aa7b47d0482c291b0175e9f
ErmeRG <emrehansonal@hotmail.com>
2026-04-30 14:52:29 +0300

    I made a if conditino for null and empty check.

--- ExponentialSearchAlgo.cs
+++ ExponentialSearchAlgo.cs
@@ -4,6 +4,9 @@ public class ExponentialSearchAlgo
 {
     public static int ExponentialSearch(int[] numbers, int target)
     {
-        
+        if (numbers == null || numbers.Length == 0)
+        {
+            return -1;
+        }
     }
 }
