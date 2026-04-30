1819e2278ab82d7f5d2687e2f258c1fd9bf43843
ErmeRG <emrehansonal@hotmail.com>
2026-04-30 14:58:19 +0300

    I made a if condition for spesific index(0) and made while loop to search our numbers position and after the find area, lets declare our borders.

--- ExponentialSearchAlgo.cs
+++ ExponentialSearchAlgo.cs
@@ -8,5 +8,21 @@ public class ExponentialSearchAlgo
         {
             return -1;
         }
+        
+        if (numbers[0] == target)
+        {
+            return 0;
+        }
+
+        int n = numbers.Length;
+        int i = 1;
+
+        while (i < n && numbers[i] <= target)
+        {
+            i*=2;
+        }
+
+        int left = i / 2;
+        int right = Math.Min(i, n - 1);
     }
 }
