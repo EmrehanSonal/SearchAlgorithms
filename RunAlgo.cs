aeb1cc876b3d5f74c1467821cd93cff134177e7f
ErmeRG <emrehansonal@hotmail.com>
2026-05-01 17:10:10 +0300

    In while loop we are going to choose an algorithm to find our target. If its not an int we can type number again.

--- RunAlgo.cs
+++ RunAlgo.cs
@@ -16,7 +16,26 @@ public class RunAlgo
     {
         int[] numbers = { 5, 15, 31, 45, 51, 55, 60, 67, 79, 82, 97, 100, 111, 120, 150 };
 
-        
+        while (true)
+        {
+            Console.WriteLine("\n=== SEARCH MENU ===");
+            Console.WriteLine("1 - Linear Search");
+            Console.WriteLine("2 - Binary Search");
+            Console.WriteLine("3 - Jump Search");
+            Console.WriteLine("4 - Interpolation Search");
+            Console.WriteLine("5 - Exponential Search");
+            Console.WriteLine("0 - Exit");
+            Console.Write("Select algorithm: ");
+
+             if (!int.TryParse(Console.ReadLine(), out int choice))
+            {
+                Console.WriteLine("Invalid choice!");
+                continue;
+            }
+        }
+
+
+
     }
 
 }
