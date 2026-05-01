85fa84d02e43eedfa28ba30189fac442500dfa89
ErmeRG <emrehansonal@hotmail.com>
2026-05-01 17:02:55 +0300

    I made a enum to choosing which algorithm you are going to use.

--- RunAlgo.cs
+++ RunAlgo.cs
@@ -1,5 +1,15 @@
 using System;
 
+public enum SearchType
+{
+    Exit = 0,
+    Linear  = 1,
+    Binary = 2,
+    Jump = 3,
+    Interpolation = 4,
+    Exponential = 5
+}
+
 public class RunAlgo
 {
     
